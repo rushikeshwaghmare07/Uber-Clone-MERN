c5c236b1ed687c689846192fe8bfe2114f00e596
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-12-17 23:33:45 +0530

    Add User model schema

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [2, "First name must be at least 2 characters long"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+});
+
+userSchema.method.generateAuthToken = function() {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  return token;
+}
+
+userSchema.method.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+}
+
+userSchema.static.hashPassword = async function(password) {
+  return await bcrypt.hash(password, 10);
+}
+
+export const userModel = mongoose.model("user", userSchema);
