35d644a9d5cec864af1bff0639046be0d67be9f1
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-12-22 09:36:53 +0530

    Fix user model typo `method to methods` in comparePassword

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -36,7 +36,7 @@ userSchema.methods.generateAuthToken = function() {
   return token;
 }
 
-userSchema.method.comparePassword = async function(password) {
+userSchema.methods.comparePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 }
 
