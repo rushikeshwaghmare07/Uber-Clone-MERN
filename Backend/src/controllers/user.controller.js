f26449d15bf73bcb1e5b84a76e864e2026403ef4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2024-12-30 23:26:43 +0530

    Implement User Login functionality

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import { userModel } from "../models/user.model.js";
 import createUser from "../services/user.service.js";
 import { validationResult } from "express-validator";
 
+// Register user
 const registerUser = async (req, res, next) => {
   const error = validationResult(req);
 
@@ -25,4 +26,39 @@ const registerUser = async (req, res, next) => {
   res.status(201).json({ token, user });
 };
 
-export { registerUser };
+// Login user
+const loginUser = async (req, res, next) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty) {
+    return res.status(400).json({ error: error.array() });
+  } 
+
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  const token = user.generateAuthToken();
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+  };
+
+  res.cookie("token", token, options);
+
+  res.status(200).json({ token, user });
+}
+
+export {
+  registerUser,
+  loginUser,
+};
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { body } from "express-validator";
-import { registerUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
@@ -11,5 +11,12 @@ router.post("/register", [
 ],
   registerUser
 );
+
+router.post("/login", [
+  body("email").isEmail().withMessage("Invalid emil"),
+  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long.")
+],
+  loginUser
+)
 
 export default router;
