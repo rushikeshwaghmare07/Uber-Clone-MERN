929b3363826b69bd0fd0c7acbcc58fd61f10c64d
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-01 21:49:52 +0530

    Add 'auth middleware'

---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+import { userModel } from "../models/user.model.js";
+import { verify } from "jsonwebtoken";
+
+export const authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized." });
+  }
+
+  try {
+    const decodedToken = verify(token, process.env.JWT_SECRET);
+
+    const user = await userModel.findById(decodedToken._id);
+    if (!user) {
+      return res.status(401).json({
+        message: "Unauthorized.",
+      });
+    }
+
+    req.user = user;
+
+    return next();
+  } catch (error) {
+    console.error("Error occurs while decoded the token:", error);
+    return res.status(500).json({ message: "Invalid token" });
+  }
+};
