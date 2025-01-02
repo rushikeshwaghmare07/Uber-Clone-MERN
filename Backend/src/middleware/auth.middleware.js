de844f6ed3f0163bed7f32be5ae2674c5115c163
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-02 23:44:35 +0530

    Update auth middleware

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -2,9 +2,18 @@ import { userModel } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 const authUser = async (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized." });
+    return res.status(401).json({
+      message: "Unauthorized.",
+    });
+  }
+
+  const isBlacklisted = await userModel.findOne({ token: token });
+  if (isBlacklisted) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
   }
 
   try {
