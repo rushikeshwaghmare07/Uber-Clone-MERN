33bf919902e8330fd0ecae9dafaf45e2215a3bc4
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-10 23:33:36 +0530

    Fix: Corrected `id` to `_id` in captain model's JWT payload

--- Backend/src/models/captain.model.js
+++ Backend/src/models/captain.model.js
@@ -70,7 +70,7 @@ const captainSchema = new mongoose.Schema({
 });
 
 captainSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_TOKEN_EXPIRY,
   });
 
