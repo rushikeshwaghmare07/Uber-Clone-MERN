bbdc74d8e03d739ee52350973b2d3c51bb2f2c6b
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-10 21:18:38 +0530

    Add auth captain middleware

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,5 +1,7 @@
 import { userModel } from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import { BlacklistToken } from "../models/blacklistToken.model.js";
+import { captainModel } from "../models/captain.model.js";
 
 const authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
@@ -9,7 +11,7 @@ const authUser = async (req, res, next) => {
     });
   }
 
-  const isBlacklisted = await userModel.findOne({ token: token });
+  const isBlacklisted = await BlacklistToken.findOne({ token: token });
   if (isBlacklisted) {
     return res.status(401).json({
       message: "Unauthorized",
@@ -35,4 +37,38 @@ const authUser = async (req, res, next) => {
   }
 };
 
-export default authUser;
+const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlacklisted = await BlacklistToken.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await captainModel.findById(decodedToken._id);
+    if (!captain) {
+      return res.status(401).json({
+        message: "Unauthorized.",
+      });
+    }
+
+    req.captain = captain;
+
+    return next();
+  } catch (error) {
+    console.error("Error occurs while decoded the token:", error);
+    return res.status(500).json({ message: "Invalid token" });
+  }
+};
+
+export {
+  authUser,
+  authCaptain,
+};
