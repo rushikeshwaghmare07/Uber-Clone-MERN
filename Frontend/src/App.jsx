e18d3662955e6e57a1267818ede002344687d48a
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-20 00:06:36 +0530

    Add captain protected wrapper

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./pages/Home";
 import UserProtectedWrapper from "./pages/UserProtectedWrapper";
 import UserLogout from "./pages/UserLogout";
 import CaptainHome from "./pages/CaptainHome";
+import CaptainProtectWrapper from "./pages/CaptainProtectedWrapper";
 
 const App = () => {
   return (
@@ -35,7 +36,14 @@ const App = () => {
             </UserProtectedWrapper>
           }
         />
-        <Route path="/captain-home" element={<CaptainHome />} />
+        <Route
+          path="/captain-home"
+          element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+          }
+        />
       </Routes>
     </div>
   );
