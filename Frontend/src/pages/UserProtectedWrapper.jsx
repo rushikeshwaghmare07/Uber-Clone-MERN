418a6822aecae00275d9bc3cc8c837926fec54ce
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-01-20 00:13:17 +0530

    Update user protected wrapper

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -1,16 +1,37 @@
 import React, { useContext, useEffect } from "react";
 import { UserDataContext } from "../context/userContext";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const UserProtectedWrapper = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
+  const { user, setUser } = useContext(UserDataContext);
 
   useEffect(() => {
     if (!token) {
       navigate("/login");
     }
   }, [token]);
+
+  axios
+    .get(`${import.meta.env.VITE_BASE_URL}/api/v1/users/profile`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        setUser(response.data.user);
+        setIsLoading(false);
+      }
+    })
+    .catch((err) => {
+      localStorage.removeItem("token");
+      navigate("/login");
+    });
 
   return <>{children}</>;
 };
